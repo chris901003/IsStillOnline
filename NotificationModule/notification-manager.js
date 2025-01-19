8718116cb08656e547431f818f48c50752e6b7ce
HongYan <a0987999103@gmail.com>
2025-01-19 11:55:43 +0800

    <IsStillOnline> #30 fix: just init firebase admin once

--- NotificationModule/notification-manager.js
+++ NotificationModule/notification-manager.js
@@ -13,27 +13,7 @@ import dotenv from 'dotenv'
 
 
 class NotificationManager {
-    constructor() {
-        dotenv.config()
-
-        const serviceAccount = {
-            type: process.env.FBA_type,
-            project_id: process.env.FBA_project_id,
-            private_key_id: process.env.FBA_private_key_id,
-            private_key: process.env.FBA_private_key.replace(/\\n/g, '\n'),
-            client_email: process.env.FBA_client_email,
-            client_id: process.env.FBA_client_id,
-            auth_uri: process.env.FBA_auth_uri,
-            token_uri: process.env.FBA_token_uri,
-            auth_provider_x509_cert_url: process.env.FBA_auth_provider_x509_cert_url,
-            client_x509_cert_url: process.env.FBA_client_x509_cert_url,
-            universal_domain: process.env.FBA_universe_domain
-        }
-
-        this.admin = admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
-        })
-    }
+    constructor() { }
 
     async sendReport(token, reports) {
         // report = [OnlineCheckerResultData]
@@ -74,5 +54,25 @@ class NotificationManager {
         }
     }
 }
+
+dotenv.config()
+
+NotificationManager.prototype.serviceAccount = {
+    type: process.env.FBA_type,
+    project_id: process.env.FBA_project_id,
+    private_key_id: process.env.FBA_private_key_id,
+    private_key: process.env.FBA_private_key.replace(/\\n/g, '\n'),
+    client_email: process.env.FBA_client_email,
+    client_id: process.env.FBA_client_id,
+    auth_uri: process.env.FBA_auth_uri,
+    token_uri: process.env.FBA_token_uri,
+    auth_provider_x509_cert_url: process.env.FBA_auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.FBA_client_x509_cert_url,
+    universal_domain: process.env.FBA_universe_domain
+}
+
+NotificationManager.prototype.admin = admin.initializeApp({
+    credential: admin.credential.cert(NotificationManager.prototype.serviceAccount)
+})
 
 export { NotificationManager }
